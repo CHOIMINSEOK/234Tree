475a9069eb2a7d2fc39e7238957b3d0c3b2fa431
lf_wannabe <cms3718@gmail.com>
2017-10-16 13:59:01 +0900

    init

---
+++ 234Tree/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  234Tree
+//
+//  Created by Minseok Choi on 16/10/2017.
+//  Copyright © 2017 Minseok Choi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
